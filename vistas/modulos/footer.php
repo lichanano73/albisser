fd5137d90ffa434b956263884a2df08cc1c82801
LichaNano <lisandro.blanco73@gmail.com>
2022-01-22 19:48:49 -0300

    Primer commit - Prj Hnos Albisser - Sis Ventas

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,12 @@
+<footer class="main-footer">
+	
+	<strong><b style="font-family: 'Apple Color Emoji';">Hnos.Albisser</b>  
+		|<?php echo date("Y"); ?>	 
+		<b style="color: #721c24">ln73</b>  
+		<b style="margin-bottom: 5%">🥾</b> 
+	</strong>
+
+	Todos los derechos reservados
+
+
+</footer>
